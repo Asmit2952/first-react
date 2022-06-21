6c574d24cd6e8f379f9b4428e2be513636d12174
Asmit2952 <asmitbm2952002@gmail.com>
2022-06-21 18:43:01 +0530

    first practice app

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# Getting Started with Create React App
+# My first React App
+Practice project following the course from freeCodeCamp.
+
+## Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/App.js
+++ src/App.js
@@ -3,22 +3,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+        <img src={logo} className="App-logo-s"width="80px" alt="logo"/>
+        <h1>Fun facts about React!</h1>
+        <ul>
+          <li>Was first released in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well over 100K stars on GitHub</li>
+          <li>Is maintained by Facebook</li>
+          <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+    </div>  
   );
 }
 
