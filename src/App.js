4966502b362086afa9d8931cdecbc65d81864a95
Asmit2952 <asmitbm2952002@gmail.com>
2022-06-22 21:31:26 +0530

    app from scratch

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import logo from './logo.svg';
+import Navbar from "./components/Navbar"
+import Main from "./components/Main"
 import './App.css';
 
 function App() {
   return (
-    <div>
-        <img src={logo} className="App-logo-s"width="80px" alt="logo"/>
-        <h1>Fun facts about React!</h1>
-        <ul>
-          <li>Was first released in 2013</li>
-          <li>Was originally created by Jordan Walke</li>
-          <li>Has well over 100K stars on GitHub</li>
-          <li>Is maintained by Facebook</li>
-          <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
+    <div className="container">
+        <Navbar />
+        <Main />
     </div>  
   );
 }
