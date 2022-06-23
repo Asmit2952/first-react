541f4dc3b191b1bd718dae8c77f60252bcc6206c
Asmit2952 <asmitbm2952002@gmail.com>
2022-06-23 14:50:23 +0530

    add fontawesome

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,13 @@
 import logo from "../images/logo.svg"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithubSquare } from '@fortawesome/free-brands-svg-icons';
 
 export default function Navbar() {
     return (
         <nav>
             <img className="App-logo" src={logo} alt="react logo" />
             <h2 className="nav-h2">ReactFacts</h2>
-            <h3 className="nav-h3">Project-1</h3>
+            <h3 className="nav-h3"><a href="https://github.com/Asmit2952/first-react" rel="noopener noreferrer" target="_blank"><FontAwesomeIcon icon={faGithubSquare} /></a></h3>
         </nav>
     )
 }
