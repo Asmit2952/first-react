7ea1ebb0265fade6002df865a95ea659f5787677
Asmit2952 <asmitbm2952002@gmail.com>
2022-06-23 11:14:33 +0530

    main component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,3 +1,15 @@
 export default function Main() {
-    return <h1>Main Component</h1>
+    return (
+        <main>
+            <h1 className="div-h1">Fun facts about React!</h1>
+            <ul className="div-ul">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 190K stars 🌟 on <a href="https://github.com/facebook/react" rel="noopener noreferrer" target="_blank">GitHub</a></li>
+                <li>Is maintained by <a href="https://opensource.fb.com/" rel="noopener noreferrer" target="_blank">Meta Open Source</a></li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+            <h3 className="div-h3">Learn more about <a href="https://reactjs.org" rel="noopener noreferrer" target="_blank">React</a></h3>
+        </main>
+    )
 }
